50c5639732911555ac031d592db54ed8fb026aa1
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-05-06 19:54:09 +0500

    add posr creat & profile

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,7 +5,9 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.PostListView.as_view(), name='index'),
+    path('posts/create', views.PostCreateView.as_view(), name='create_post'),
+    path('profile/<slug:username>/', views.ProfileDetailView.as_view(), name='profile'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
     path('category/<slug:category_slug>/', views.category_posts,
          name='category_posts'),
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,15 +1,41 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import CreateView, DetailView, ListView
+from django.urls import reverse_lazy
 
 from blog.models import Category, Post
 
 
-def index(request):
-    template = 'blog/index.html'
-    post_list = Post.published.select_related('category')[0:5]
-    context = {
-        'post_list': post_list
-    }
-    return render(request, template, context)
+User = get_user_model()
+
+
+class PostListView(ListView):
+    model = Post
+    ordering = 'id'
+    paginate_by = 10
+    template_name = 'blog/index.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = '__all__'
+    template_name = 'blog/create.html'
+    success_url = reverse_lazy('blog:index')
+
+
+class ProfileDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'blog/profile.html'
+
+
+# def index(request):
+#     template = 'blog/index.html'
+#     post_list = Post.published.select_related('category')[0:5]
+#     context = {
+#         'post_list': post_list
+#     }
+#     return render(request, template, context)
 
 
 def post_detail(request, pk):
