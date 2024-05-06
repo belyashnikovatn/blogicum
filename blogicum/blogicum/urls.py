b22a6eb542c47e344d0d219d7c17b278adbc9616
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-05-06 18:32:38 +0500

    add user registration

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import include, path, reverse_lazy
+from django.views.generic.edit import CreateView
 
 
 handler403 = 'core.views.csrf_failure'
@@ -25,4 +27,14 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
 ]
