7004437fb1a1c14fbca51ed00077cfa4ee06bd33
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-05-12 23:16:52 +0500

    add one more try for PostDeleteView

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -6,7 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.db.models.base import Model as Model
 from django.db.models.query import QuerySet
 from django.db.models import Count
-from django.http.response import HttpResponseRedirect
+from django.http import HttpRequest
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
@@ -82,7 +83,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse(
+        return reverse_lazy(
             'blog:profile',
             kwargs={'username': self.request.user}
         )
@@ -92,10 +93,9 @@ class PostUpdateView(OnlyAuthorMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/create.html'
-    redirect_field_name = 'blog:index'
 
     def get_success_url(self):
-        return reverse(
+        return reverse_lazy(
             'blog:post_detail',
             kwargs={'pk': self.kwargs['pk']}
         )
@@ -106,12 +106,30 @@ class PostDeleteView(OnlyAuthorMixin, DeleteView):
     form_class = PostForm
     template_name = 'blog/create.html'
 
+    # def get(self, request, *args, **kwargs):
+    #     self.object = self.get_object()
+    #     context = self.get_context_data(object=self.object)
+    #     return self.render_to_response(context)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     # instance = get_object_or_404(Post, self.kwargs['pk'])
+    #     # context['form'] = PostForm(instance=instance)
+    #     return context
+
+    # def form_valid(self, form):
+    #     return super().form_valid(form)
+
+    # def get_form(self, form_class=PostForm):
+    #     """Return an instance of the form to be used in this view."""
+    #     if form_class is None:
+    #         form_class = self.get_form_class()
+    #     return form_class(**self.get_form_kwargs())
+
     def get_success_url(self):
-        return reverse(
+        return reverse_lazy(
             'blog:profile',
-            kwargs={
-                'username': get_object_or_404(User, id=self.request.user.id)
-            }
+            kwargs={'username': self.request.user}
         )
 
 
