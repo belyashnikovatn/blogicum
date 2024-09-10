14ab5161fb68f2b1a4e447bb68c4241447d46262
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-09-10 09:47:08 +0500

    just main.yml

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -2,9 +2,10 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 load_dotenv()
 
-from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
