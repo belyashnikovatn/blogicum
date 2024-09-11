d1763c67605575f9a8716ab9e6a15fc4102ad166
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-09-11 08:19:46 +0500

    add media_volume & change settings & urls

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -131,7 +131,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / '/media'
+# MEDIA_ROOT = BASE_DIR / '/media'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
